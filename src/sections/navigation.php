ed95382891c767bd9f2c58a231d38b3aeb0e57c9
jon <jgkerkhoven@gmail.com>
2017-11-21 10:59:37 +0100

    create navigation

--- src/sections/navigation.php
+++ src/sections/navigation.php
@@ -1,9 +1,9 @@
 <nav class="navigation">
-	<ul class="menu"> <!--tofo -->
-		<li><i class="fa fa-arrow-up"></i></li>
+	<ul class="menu">
+		<span class="menu__arrow menu__arrow--up"></span>
 		<li class="menu__item"></li>
+		<li class="menu__item menu__item--active"></li>
 		<li class="menu__item"></li>
-		<li class="menu__item"></li>
-		<li><i class="fa fa-arrow-down"></i></li>
+		<span class="menu__arrow menu__arrow--down"></span>
 	</ul>
 </nav>
