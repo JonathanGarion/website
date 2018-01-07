c78fcad72b2d485ef8940d344906c454a0beb1f9
jon <jgkerkhoven@gmail.com>
2018-01-07 15:32:54 +0100

    add js

--- src/js/project.js
+++ src/js/project.js
@@ -3,27 +3,28 @@ let project = () => {
 	let projectButton = document.querySelector('.header__corner');
 	let body = document.querySelector('.body');
 	let navItem = document.querySelector('.menu__item');
-	
-	projectButton.addEventListener('click', toggleProject());
-	navItem.addEventListener('click', closeProject());
-
+	console.log(projectButton);
+		
 	let toggleProject = () => {
 		if (projectItem.classList.contains('.project--open')) {
-			projectItem.classList.replace('.project--open, .project--closed');
+			projectItem.classList.replace('.project--open', '.project--closed');
 		}
 		if (projectItem.classList.contains('.project--closed')) {
-			projectItem.classList.replace('.project--closed, .project--open');
+			projectItem.classList.replace('.project--closed', '.project--open');
 		}
 		body.classList.toggle('.body--noscroll');	
 		console.log('after toggleProject');	
 	}
-
+	
 	let closeProject = () => {
 		console.log('closeProject');
 		if (projectItem.classList.contains('.project--open')) {
-			projectItem.classList.replace('.project--open, .project--closed');
+			projectItem.classList.replace('.project--open', '.project--closed');
 			body.classList.remove('.body--noscroll');		
 		}
 	}
+
+	projectButton.addEventListener('click', toggleProject());
+	navItem.addEventListener('click', closeProject());
 }
 project();
